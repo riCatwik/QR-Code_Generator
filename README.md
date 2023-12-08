841115b916db46c075b413b40c7ff557e00ea0b5
Ritwik Das <59201175+riCatwik@users.noreply.github.com>
2023-12-08 02:47:05 +0100

    Committed: Fri, Dec  8, 2023  2:47:05 AM from Ritwik-C2N

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# QR-Code_Generator
-A Python-based tool to swiftly generate customizable QR codes for any URL, streamlining the process of linking to websites in the physical world.
+# QR Code Generator
+
+This QR Code Generator is a simple yet powerful Python tool that creates QR codes with the option to customize the color, add a logo, and include artistic text. Designed to be easy to use and modify, it's perfect for generating QR codes for personal or professional use.
+
+## Features
+
+- Generate QR codes for any URL.
+- Customize QR code color and background.
+- Add a logo to the center of the QR code.
+- Include custom text beneath the QR code with artistic effects.
+
+## Requirements
+
+- Python 3.x
+- Pillow library
+- qrcode library
+
+## Installation
+
+To run this script, you will need Python installed on your system. If you don't have the required libraries (`Pillow` and `qrcode`), install them using pip:
+
+```bash
+pip install pillow qrcode
+```
+
+## Usage
+
+To generate a QR code, simply execute the script with Python and the generated QR code will be saved as an image file:
+
+```bash
+python generate_qr.py
+```
+
+The default settings are:
+
+- URL: `https://ritwikdas.gitlab.io`
+- Logo: `favicon-2.png` (Place your logo in the script's directory or specify the path)
+- Text: "RITWIK DAS"
+- QR Color: Navy
+- Background: White
+
+You can customize these settings directly in the script.
+
+## Customization
+
+To customize your QR code, modify the parameters in the `generate_qr` function call at the bottom of the script:
+
+- `url`: The URL that the QR code will point to.
+- `logo_path`: The path to the logo image you want to embed in the QR code.
+- `text`: The text you want to display beneath the QR code.
+- `logo_size`: The size of the logo within the QR code.
+- `text_font_size`: The font size of the text beneath the QR code.
+- `qr_color`: The color of the QR code.
+- `background`: The background color of the QR code.
+
+## Contributing
+
+Contributions are welcome! For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is open source and available under the [MIT License](LICENSE).
---
+++ generate_qr.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 2023-12-08 at 01:56:36 CET
+
+Machine: ritwikdas @ C2N_Linux 
+OS: Red Hat® Enterprise Linux® 8.6 (RHEL LF Unix)
+RHEL License Holder: Ritwik Das (Individual license)
+License Information: RHDS 10x7xx17, Beta Access 10x7xx19
+
+@author: Ritwik DAS (ritwik.das29@gmail.com)
+"""
+
+import qrcode
+from PIL import Image, ImageDraw, ImageFont
+
+
+def generate_qr(
+    url,
+    logo_path=None,
+    text=None,
+    logo_size=80,
+    text_font_size=20,
+    qr_color="purple",
+    background="white",
+):
+    # Generate QR code
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    # Create an image from the QR Code instance
+    img = qr.make_image(fill_color=qr_color, back_color=background).convert("RGB")
+
+    if logo_path:
+        # Add logo to the QR code
+        logo = Image.open(logo_path)
+        logo.thumbnail((logo_size, logo_size))
+        logo_pos = (
+            (img.size[0] - logo.size[0]) // 2,
+            (img.size[1] - logo.size[1]) // 2,
+        )
+        img.paste(logo, logo_pos, mask=logo)
+
+    if text:
+        # Add artistic text under the QR code
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("arial.ttf", text_font_size)
+
+        # Calculate the bounding box for the text
+        text_box = draw.textbbox((0, 0), text, font=font)
+        text_width = text_box[2] - text_box[0]
+        text_height = text_box[3] - text_box[1]
+
+        # Calculate the position for the text to appear at the bottom center
+        text_x = (img.size[0] - text_width) // 2
+        text_y = img.size[1] - text_height - 10  # 10 pixels from the bottom
+
+        # Add some artistic styling (e.g., shadow effect)
+        shadow_color = "grey"
+        # Draw shadow first
+        draw.text((text_x + 2, text_y + 2), text, font=font, fill=shadow_color)
+        # Draw text
+        draw.text((text_x, text_y), text, font=font, fill=qr_color)
+
+    # Save it somewhere, change the path as needed
+    img.save("website_qr.png")
+
+
+## ======================================================== ##
+## ======================================================== ##
+
+
+## Define the website URL
+url = "https://ritwikdas.gitlab.io"
+
+## Path to logo image (optional)
+logo_path = "favicon-2.png"
+
+## Text to add under the QR code (optional)
+text = "RITWIK DAS"
+
+## Call the generate_qr function with the given URL, logo path, and text
+## You can now specify the logo size, text font size, QR color, and background color as needed
+generate_qr(
+    url,
+    logo_path=logo_path,
+    text=text,
+    logo_size=100,
+    text_font_size=35,
+    qr_color="navy",
+    background="white",
+)
