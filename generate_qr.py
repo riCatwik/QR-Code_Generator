ec03e4e9678136ea3f8108ef4d7e2cf1de25b40a
Ritwik Das <59201175+riCatwik@users.noreply.github.com>
2023-12-08 05:00:13 +0100

    Committed: Fri, Dec  8, 2023  5:00:13 AM from Ritwik-C2N

--- generate_qr.py
+++ generate_qr.py
@@ -11,6 +11,7 @@ License Information: RHDS 10x7xx17, Beta Access 10x7xx19
 @author: Ritwik DAS (ritwik.das29@gmail.com)
 """
 
+
 import qrcode
 from PIL import Image, ImageDraw, ImageFont
 
@@ -20,84 +21,93 @@ def generate_qr(
     name="website",
     logo_path=None,
     text=None,
-    logo_size=80,
-    text_font_size=20,
-    qr_color="purple",
+    logo_size=100,
+    text_font_size=40,
+    qr_color="navy",
     background="white",
+    canvas_color="navy",
+    border_size=10,
 ):
     # Generate QR code
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
         box_size=10,
-        border=4,
+        border=1,
     )
     qr.add_data(url)
     qr.make(fit=True)
 
     # Create an image from the QR Code instance
-    img = qr.make_image(fill_color=qr_color, back_color=background).convert("RGB")
+    qr_img = qr.make_image(fill_color=qr_color, back_color=background).convert("RGB")
 
+    # Add logo to the QR code if a path is provided
     if logo_path:
-        # Add logo to the QR code
         logo = Image.open(logo_path)
         logo.thumbnail((logo_size, logo_size))
         logo_pos = (
-            (img.size[0] - logo.size[0]) // 2,
-            (img.size[1] - logo.size[1]) // 2,
+            (qr_img.size[0] - logo.size[0]) // 2,
+            (qr_img.size[1] - logo.size[1]) // 2,
         )
-        img.paste(logo, logo_pos, mask=logo)
+        qr_img.paste(logo, logo_pos, mask=logo)
 
-    if text:
-        # Add artistic text under the QR code
-        draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("arial.ttf", text_font_size)
+    # Calculate the total canvas size, accounting for the border
+    canvas_width = qr_img.size[0] + 2 * border_size
+    canvas_height = qr_img.size[1] + 2 * border_size + text_font_size
 
-        # Calculate the bounding box for the text
-        text_box = draw.textbbox((0, 0), text, font=font)
-        text_width = text_box[2] - text_box[0]
-        text_height = text_box[3] - text_box[1]
+    # Create a larger canvas for QR code and text
+    canvas = Image.new("RGB", (canvas_width, canvas_height), canvas_color)
 
-        # Calculate the position for the text to appear at the bottom center
-        text_x = (img.size[0] - text_width) // 2
-        text_y = img.size[1] - text_height - 10  # 10 pixels from the bottom
+    # Paste the QR image onto the canvas, centered at the top with a border
+    qr_position = (border_size, border_size)
+    canvas.paste(qr_img, qr_position)
 
-        # Add some artistic styling (e.g., shadow effect)
-        shadow_color = "grey"
-        # Draw shadow first
-        draw.text((text_x + 2, text_y + 2), text, font=font, fill=shadow_color)
-        # Draw text
-        draw.text((text_x, text_y), text, font=font, fill=qr_color)
+    # Initialize drawing context for text
+    draw = ImageDraw.Draw(canvas)
+    font = ImageFont.truetype("arial.ttf", text_font_size)
 
-    # Save it somewhere, change the path as needed
-    img.save(f"QR_{name}.png")
+    # Use textbbox to calculate the bounding box for the text
+    text_box = draw.textbbox((0, 0), text, font=font)
+    text_width = text_box[2] - text_box[0]
+    text_height = text_box[3] - text_box[1]
+
+    # Calculate text position for center alignment within the blue box
+    text_x = (canvas_width - text_width) // 2
+    text_y = canvas_height - border_size - (text_font_size + text_height)//1.8
+
+    # Draw the text
+    draw.text((text_x, text_y), text, font=font, fill=background)
+
+    # Save the resulting image
+    canvas.save(f"QR_{name}.png")
 
 
 ## ======================================================== ##
 ## ======================================================== ##
 
 
-## Define the full website URL (will be redirected to this link)
+# Define the full website URL (will be redirected to this link)
 url = "https://ritwikdas.gitlab.io"
 
-## Naming convention of the website URL
+# Naming convention of the website URL
 name = "ritwikdas.gitlab.io"
 
-## Path to logo image (optional)
-logo_path = "favicon-2.png"
+# Path to logo image (optional)
+logo_path = "favicon-2.png"  # Replace with the path to your logo image
 
-## Text to add under the QR code (optional)
+# Text to add under the QR code (optional)
 text = "RITWIK DAS"
 
-## Call the generate_qr function with the given URL, logo path, and text
-## You can now specify the logo size, text font size, QR color, and background color as needed
+# Call the generate_qr function with the given URL and text
 generate_qr(
     url,
     name,
     logo_path=logo_path,
     text=text,
     logo_size=100,
-    text_font_size=35,
+    text_font_size=50,
     qr_color="navy",
     background="white",
+    canvas_color="navy",
+    border_size=10,
 )
